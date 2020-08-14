96ca6a5840cea0d6df936c1b4d117723aebf2f4e
hanifalkauni <zechnider@gmail.com>
2020-08-14 23:33:16 +0700

    for deploying to heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,104 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE HTML>
+<!--
+    Identity by HTML5 UP
+    html5up.net | @n33co
+    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        
+        <title>Muhammad Hanif</title>
+        
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <!--[if lte IE 8]><script src="assets/js/html5shiv.js"></script><![endif]-->
+        <link rel="stylesheet" href="{{asset('assets/css/main.css')}}" />
+        <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+        <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+        <noscript><link rel="stylesheet" href="{{asset('assets/css/noscript.css')}}" /></noscript>
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+    <body class="is-loading">
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+        <!-- Wrapper -->
+            <div id="wrapper">
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                <!-- Main -->
+                    <section id="main">
+                        
+                        <header>
+                            
+                            <span class="avatar">
+                                <img src="https://scontent.fsub3-1.fna.fbcdn.net/v/t1.0-9/31768713_1839674299417086_8239926269551050752_n.jpg?_nc_cat=106&_nc_sid=174925&_nc_eui2=AeEdFt57rHknpIsLQrcW1jFE4ejEUUWlaqbh6MRRRaVqpj9EW2u-uK1Z5Wi3Rq7PeyROkE1IpNl1Jd-7Cl0VOXE6&_nc_ohc=y9kZI6TYSdgAX_pNr12&_nc_ht=scontent.fsub3-1.fna&oh=49f129b5a357b409a641a3b32a006434&oe=5F53574E" style="height: 250px; width: 250px;" alt="Avatar" />
+                            </span>
+                            
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                            
+                            <h1>Muhammad Hanif</h1>
+                            
+
+                            
+                            <p>Back-End Developer</p>
+                            
+                        </header>
+                        <hr />
+                        
+
+                        
+                        <p>PHP - Laravel - MySql</p>
+                        <hr />
+                        
+
+                        
+                        <p>
+                            Sumbawa - Nusa Tenggara Barat - Indonesia
+                             | 081282283848 
+                        </p>
+                        
+
+                        <footer>
+                            
+                            <ul class="icons">
+                                
+                                <li><a href="mailto:zechnider%40gmail.com" class="fa-envelope" title="Email">Email</a></li>
+                                
+
+                                
+                                <li><a href="https://twitter.com/hanifalkauni" class="fa-twitter" target="_blank" title="Twitter">Twitter</a></li>
+                                
+
+                                
+                                <li><a href="https://facebook.com/hanifalkauni" class="fa-facebook" target="_blank" title="Facebook">Facebook</a></li>
+                                
+
+                                
+                                <li><a href="https://www.linkedin.com/in/muhammad-hanif-7b99b5143/" class="fa-linkedin" target="_blank"  title="LinkedIn">LinkedIn</a></li>
+                                
+                            </ul>
+                            
+                        </footer>
+                    </section>
+
+                <!-- Footer -->
+                    <footer id="footer">
+                        <ul class="copyright">
+                            
+                            <li>&copy; Muhammad Hanif</li>
+                            
+                            <li>Design: <a href="//html5up.net">HTML5 UP</a></li>
+                        </ul>
+                    </footer>
+
             </div>
-        </div>
+
+        <!-- Scripts -->
+            <!--[if lte IE 8]><script src="assets/js/respond.min.js"></script><![endif]-->
+            <script>
+                if ('addEventListener' in window) {
+                    window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-loading\b/, ''); });
+                    document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
+                }
+            </script>
+
+            
     </body>
 </html>
