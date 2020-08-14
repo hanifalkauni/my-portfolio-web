00c3cc1a50e9f2d2e4386bd7f7f7c1b95db699ca
hanifalkauni <zechnider@gmail.com>
2020-08-14 23:44:35 +0700

    commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE HTML>
+<!--
+    Identity by HTML5 UP
+    html5up.net | @n33co
+    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+    <head>
+        
+        <title>Muhammad Hanif</title>
+        
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="front/main.css" />
+        <noscript><link rel="stylesheet" href="front/noscript.css" /></noscript>
+    </head>
+    <body class="is-loading">
+
+        <!-- Wrapper -->
+            <div id="wrapper">
+
+                <!-- Main -->
+                    <section id="main">
+                        
+                        <header>
+                            
+                            <h1>MY PORTFOLIO</h1>
+                        
+                        </header>
+                        <hr />
+                        
+                        <p>- SKILL PROGRAMMING -<br>
+                            PHP<br>
+                            Javascript<br>
+                            Laravel Framework<br>
+                            Source Code Management using Github<br>
+                            <a href="https://www.dicoding.com/certificates/VJLX1O7KJX72">S.O.L.I.D Principle Programming</a>
+                        </p>
+                        <hr />
+            
+                        <p>- OTHER SKILL-<br>
+                            Graphic Design (CorelDraw)<br>
+                            Video Editing (Adobe After Effect)<br>
+                            Office Working (Ms.Office)<br>
+                            Public Speaking
+                        </p>
+                        
+                        <hr>
+
+                        <footer>
+                            
+                           
+                        </footer>
+                    </section>
+
+                <!-- Footer -->
+                    <footer id="footer">
+                        <ul class="copyright">
+                            
+                            <li>&copy; Muhammad Hanif</li>
+                            
+                            <li>Design: <a href="//html5up.net">HTML5 UP</a></li>
+                        </ul>
+                    </footer>
+
+            </div>
+
+        <!-- Scripts -->
+           
+            <script>
+                if ('addEventListener' in window) {
+                    window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-loading\b/, ''); });
+                    document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
+                }
+            </script>
+
+            
+    </body>
+</html>
